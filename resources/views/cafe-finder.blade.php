0e19cb5c60e64e39e030e8ce8594f0f171d23c6e
Nishikawa miyu <nishikawamiyu@Nishikawas-MacBook-Air.local>
2026-06-02 23:52:11 +0900

    Add new theme-colored coffee favicon

--- resources/views/cafe-finder.blade.php
+++ resources/views/cafe-finder.blade.php
@@ -26,6 +26,7 @@
 
     <!-- Theme Color & Icons -->
     <meta name="theme-color" content="#2ABFBF">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
