d638ad030c27747115cf1592183ec77db03ae627
Nishikawa miyu <nishikawamiyu@Nishikawas-MacBook-Air.local>
2026-06-02 23:42:44 +0900

    Remove coffee favicon from tab

--- resources/views/cafe-finder.blade.php
+++ resources/views/cafe-finder.blade.php
@@ -26,9 +26,7 @@
 
     <!-- Theme Color & Icons -->
     <meta name="theme-color" content="#2ABFBF">
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-    <link rel="icon" href="/favicon.ico">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     @viteReactRefresh
